78a3ea6b557bcedcc7f0dd97b8d968e75510da37
Lux Miranda <19409711+LuxMiranda@users.noreply.github.com>
2022-07-13 10:32:50 -0400

    Approaching basic network simulation

--- simulate.py
+++ simulate.py
@@ -9,38 +9,74 @@ pA = 0.5
 pB = 0.5 + epsilon
 
 # Simple belief-updating conditionalization using Baye's rule
-def strictConditionalization(credence):
-    # Select arm based on credence
-    arm = 'A' if credence < 0.5 else 'B'
-    # Pull the arm
-    armSuccessRate = pA if arm == 'A' else pB
-    result = 'success' if np.random.rand() < armSuccessRate else 'fail'
-    print(f'Pulling arm {arm}')
-    print(f'Result: {result}')
-    # If we pulled arm A, we have no information to update our credence for B
-    if arm == 'A':
-        return credence
-    # Else, if we pulled arm B
-    else:
-        # Update our credence for B in light of the new evidence with Bayes rule
-        pEH    = (pB if result == 'success' else 1-pB) # P(E|H)
-        pH     = credence                    # P(H)
-        pEnotH = (pA if result == 'success' else 1-pA) # P(E|~H)
-        pnotH  = 1 - pH                      # P(~H)
-        return (pEH*pH)/(pEH*pH + pEnotH*pnotH)
+#                 P(E|H)P(H)
+# P(H|E) = -------------------------
+#          P(E|H)P(H) + P(E|~H)P(~H)
+def strictConditionalization(credence, result):
+    # Update our credence for B in light of the new evidence with Bayes rule
+    pEH    = (pB if result == 'success' else 1-pB) # P(E|H)
+    pH     = credence                              # P(H)
+    pEnotH = (pA if result == 'success' else 1-pA) # P(E|~H)
+    pnotH  = 1 - pH                                # P(~H)
+    return (pEH*pH)/(pEH*pH + pEnotH*pnotH)
 
-def jefferyConditionalization():
+def jeffreyConditionalization():
     return
 
 # Base Agent class
 class Agent:
-    def __init__(self):
-        # Credence is randomly initialized along a uniform distribution
-        self.credence = np.random.rand()
-        self.conditionalization = strictConditionalization
+    def __init__(self, 
+            # Randomly initialize credence along a uniform distribution
+            initial_credence=np.random.rand(),
+            conditionalization = 'strict'
+        ):
+        # Credence := Agent's belief of the probability that B is correct
+        self.credence = initial_credence
+        # Belief-updating function
+        if conditionalization == 'strict':
+            self.conditionalization = strictConditionalization
+        elif conditionalization == 'jeffrey':
+            self.conditionalization = jeffreyConditionalization
+        # Most recent pull result, used for sharing with other agents 
+        self.pullResult = (None,None)
+        print(f'Initial credence: {self.credence}')
+            
+    # Update credence given the result of a B arm pull
+    def update_credence(self, result):
+        self.credence = self.conditionalization(self.credence, result)
+        print(f'New credence:{self.credence}')
 
-    def update(self):
-        self.credence = self.conditionalization(self.credence)
+    # Choose an arm to pull, pull it, and update credence if B was pulled
+    def update_on_self(self):
+        # Select arm based on credence
+        arm = 'A' if self.credence < 0.5 else 'B'
+        # Pull the arm
+        armSuccessRate = pA if arm == 'A' else pB
+        result = 'success' if np.random.rand() < armSuccessRate else 'fail'
+        print(f'Pulling arm {arm}')
+        print(f'Result: {result}')
+        # If we pulled arm B, we have evidence to update our credence for B
+        if arm == 'B':
+            self.update_credence(result)
+        # Update pullResult to share with other agents
+        self.pullResult = (arm,result)
 
-a = Agent()
+    # Fetch pull results from all neighbors and update credence
+    def update_on_neighbors(self):
+        # Get the round's pull results from each neighbor
+        for neighbor in self.neighbors:
+            arm,result = neighbor.pullResult
+            # Update self credence if neighbor pulled B
+            if arm == 'B':
+                self.update_credence(result)
 
+def main():
+    a = Agent()
+    a.update_on_self()
+    a.update_on_self()
+    a.update_on_self()
+    a.update_on_self()
+    a.update_on_self()
+
+if __name__ == '__main__':
+    main()
