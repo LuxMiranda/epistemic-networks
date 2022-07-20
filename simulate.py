9469df3bf6037ba0f50e4487afdc4fe1d60e4228
Lux Miranda <19409711+LuxMiranda@users.noreply.github.com>
2022-07-20 14:54:35 -0400

    Initials of first real robust replication

--- simulate.py
+++ simulate.py
@@ -1,5 +1,10 @@
 import numpy as np
 import epnets as ep
+import pandas as pd
+from multiprocessing import Pool, cpu_count
+
+# Leave a couple of threads for system functions
+N_THREADS = cpu_count() - 2
 
 def make_prepolarized_agents(n_agents, epsilon, n_pulls, m_mistrust):
     half = int(float(n_agents)/2.0)
@@ -21,20 +26,30 @@ class Weatherall_OConnor_2021_Fig_3():
         self.n_agents = 20
         self.epsilon  = 0.01
         self.n_pulls  = 50
+        self.n_repetitions = 50
 
     def run(self, m_mistrust):
+        print(f'Simulating Weatherall & O\'Connor (2021) Fig. 3 with m_mistrust={m_mistrust}')
         agents = make_prepolarized_agents(
                 self.n_agents, 
                 self.epsilon, 
                 self.n_pulls, 
                 m_mistrust)
         net = ep.simulate(agents=agents, net_structure='complete')
-        print(net.to_dict())
+        return net.to_dict()
+
+    def replicate(self):
+        with Pool(N_THREADS) as p:
+            results = p.map(self.run, [1])
+        df = pd.DataFrame(results)
+        df.index.name = 'Run'
+        return df
 
 
 def run_replications():
-    sim = Weatherall_OConnor_2021_Fig_3()
-    sim.run(1)
+    sim  = Weatherall_OConnor_2021_Fig_3()
+    data = sim.replicate()
+    print(data)
     return
 
 def main():
